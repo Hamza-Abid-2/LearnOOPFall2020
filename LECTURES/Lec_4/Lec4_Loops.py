35f2e6c871886955634c53d04a07f56bdc5a01dc
Hamza Abid Waraich <hamzah2490@gmail.com>
2020-09-05 19:27:07 +0500

    Add files via upload

---
+++ LECTURES/Lec_4/Lec4_Loops.py
@@ -0,0 +1,31 @@
+#Loops in python
+#Loops are  te repetitive structures which are used for repeated execution of line of code
+#
+
+
+
+#While Loop
+#Printing first ten natural numbers
+
+counter = 1
+
+while counter <= 10:
+    print(counter)
+    print("Allah")
+    counter = counter + 1
+print("Allah is Great")
+
+
+
+
+
+
+print("=============================================================")
+
+x = range(1,10)
+
+for number in x:
+    print(number)
+
+
+
